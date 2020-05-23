bcef5b2f835617ee582ad4a94763193264e95014
Soner Kocak <soner.kocak@inavitas.com>
2020-05-23 14:55:47 +0300

    ilk commit.

--- src/Magaza.js
+++ src/Magaza.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const saatSubject = new ReplaySubject(1);
 
-export function saatGuncelle(user) {
+export function saatGuncelle() {
     axios.get('http://worldtimeapi.org/api/timezone/Europe/Istanbul')
         .then(({data}) => {
             saatSubject.next(data.datetime);
